8a037b6dd0d00bef2a1916c1d99d35250693c168
CarlosSouzapjr <josecarlossouzapjr@gmail.com>
2026-05-22 23:41:23 -0300

    fix: Correção de bug de solução não variar

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ from game_logic import GameLogic
 from genetic_algorithm import GeneticAlgorithm
 import copy
 # Define o tamanho do puzzle (ex: 3x3)
-tamanho_puzzle = 3
+tamanho_puzzle = 5
 
 # Cria e embaralha o jogo original
 jogo = GameLogic(tamanho_puzzle)
 print("Tabuleiro Original (Resolvido):")
 print(jogo.board)
 
-# Embaralha com 20 movimentos aleatórios (para não ficar impossível nos testes iniciais)
-jogo.randomize(moves=20)
+jogo.randomize(moves=50) # Embaralha o tabuleiro com 50 movimentos aleatórios
 print("\nTabuleiro Embaralhado (Problema Inicial):")
 print(jogo.board)
 print("-" * 30)
 
 jogo = copy.deepcopy(jogo)
 
-# Configura os hiperparâmetros do Algoritmo Genético
-# DICA: Ajuste esses valores durante os testes
+# Hiperparâmetros do Algoritmo Genético
 tamanho_populacao = 100
-tamanho_cromossomo = 30   # Um pouco maior que o número de embaralhamentos
+tamanho_cromossomo = 100   # Número máximo de movimentos permitidos na solução
 taxa_mutacao = 0.05       # 5% de chance de mutação por gene
-max_geracoes = 200
+max_geracoes = 500
 
 # Instancia o AG
 ag = GeneticAlgorithm(
@@ -46,7 +44,6 @@ if melhor_solucao is None:
     print("\nO algoritmo não encontrou uma solução com esses parâmetros.")
 else:
     # Jogar os movimentos encontrados para verificar o resultado
-    print("\nAplicando a melhor sequência de movimentos no tabuleiro embaralhado...")
     jogo.play_moves(melhor_solucao)
     print("Tabuleiro após aplicar os movimentos:")
     print(jogo.board)
